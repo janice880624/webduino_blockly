4b1ea94d274df459f20f2d3474cece226dc363d7
janice880624 <47550853+janice880624@users.noreply.github.com>
2021-01-06 00:14:51 +0800

    0106

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 +(function (window, document) {
     function line(v1, v2, v3) {
-    var obj = {};
-    if (v1 != "''" || v1.length > 0) {
-    obj.value1 = v1;
+
+        var obj = {};
+
+        if (v1 != "''" || v1.length > 0) {
+            obj .value1 = v1;
+        }
+
+        if (v2 != "''" || v1.length > 0) {
+            obj.value2 = v2;
+        }
+
+        if (v3 != "''" || v1.length > 0) {
+            obj.value3 = v3;
+        }
+
     }
-    if (v2 != "''" || v1.length > 0) {
-        obj.value2 = v2;
-    }
-    if (v3 != "''" || v1.length > 0) {
-    obj.value3 = v3;
-    }
+
 }(window, window.document));
