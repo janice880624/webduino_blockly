6b0bdfbeb8b20f6df979862523acf71833753a18
janice880624 <47550853+janice880624@users.noreply.github.com>
2021-01-06 00:23:22 +0800

    code

--- blockly/javascript.js
+++ blockly/javascript.js
@@ -3,6 +3,13 @@ Blockly.JavaScript['linebot'] = function(block) {
     var value_channelsecret = Blockly.JavaScript.valueToCode(block, 'channelSecret', Blockly.JavaScript.ORDER_ATOMIC);
     var value_token = Blockly.JavaScript.valueToCode(block, 'Token', Blockly.JavaScript.ORDER_ATOMIC);
     // TODO: Assemble JavaScript into code variable.
-    var code = '...;\n';
+    // var code = '...;\n';
+
+    var bot = linebot({
+        channelId: value_channelid,
+        channelSecret: value_channelsecret,
+        channelAccessToken: value_token
+    });
+
     return code;
 };
