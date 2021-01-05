dd3db4a6d57b8e60cdb3435b338b45f8f3de715e
janice880624 <47550853+janice880624@users.noreply.github.com>
2021-01-06 00:06:18 +0800

    line

---
+++ blockly/blocks.js
@@ -0,0 +1,21 @@
+Blockly.Blocks['line'] = {
+    init: function() {
+      this.appendValueInput("channelId")
+          .setCheck("String")
+          .setAlign(Blockly.ALIGN_RIGHT)
+          .appendField("channelId:");
+      this.appendValueInput("channelSecret")
+          .setCheck("String")
+          .setAlign(Blockly.ALIGN_RIGHT)
+          .appendField("channelSecret:");
+      this.appendValueInput("Token")
+          .setCheck("String")
+          .setAlign(Blockly.ALIGN_RIGHT)
+          .appendField("Token:");
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(165);
+   this.setTooltip("")  ;
+   this.setHelpUrl("");
+    }
+};
---
+++ blockly/javascript.js
@@ -0,0 +1,8 @@
+Blockly.JavaScript['line'] = function(block) {
+    var value_channelid = Blockly.JavaScript.valueToCode(block, 'channelId', Blockly.JavaScript.ORDER_ATOMIC);
+    var value_channelsecret = Blockly.JavaScript.valueToCode(block, 'channelSecret', Blockly.JavaScript.ORDER_ATOMIC);
+    var value_token = Blockly.JavaScript.valueToCode(block, 'Token', Blockly.JavaScript.ORDER_ATOMIC);
+    // TODO: Assemble JavaScript into code variable.
+    var code = '...;\n';
+    return code;
+};
