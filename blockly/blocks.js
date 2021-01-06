f5eae767114cadb8fb911041c51d3c25ddeda79e
janice kuo <linakuo880624@gmail.com>
2021-01-06 17:45:14 +0800

    Update blocks.js

--- blockly/blocks.js
+++ blockly/blocks.js
@@ -3,18 +3,18 @@ Blockly.Blocks['linebot'] = {
     this.appendValueInput("id")
         .setCheck("null")
         .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField("id")
-        .setOutput(true, 'String');
+        .appendField("id");
+        // .setOutput(true, 'String');
     this.appendValueInput("serect")
         .setCheck("null")
         .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField("serect")
-        .setOutput(true, 'String');
+        .appendField("serect");
+        // .setOutput(true, 'String');
     this.appendValueInput("token")
         .setCheck("null")
         .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField("token")
-        .setOutput(true, 'String');
+        .appendField("token");
+        // .setOutput(true, 'String');
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(180);
