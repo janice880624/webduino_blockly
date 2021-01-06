b5dd6141ba013741e28d52347ce7cef2ce3fdc80
janice kuo <linakuo880624@gmail.com>
2021-01-06 17:52:11 +0800

    1

--- blockly/blocks.js
+++ blockly/blocks.js
@@ -4,22 +4,22 @@ Blockly.Blocks['linebot'] = {
         .setCheck("null")
         .setAlign(Blockly.ALIGN_RIGHT)
         .appendField("id");
-        // .setOutput(true, 'String');
+
     this.appendValueInput("serect")
         .setCheck("null")
         .setAlign(Blockly.ALIGN_RIGHT)
         .appendField("serect");
-        // .setOutput(true, 'String');
+
     this.appendValueInput("token")
         .setCheck("null")
         .setAlign(Blockly.ALIGN_RIGHT)
         .appendField("token");
-        // .setOutput(true, 'String');
+
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(180);
- this.setTooltip("");
- this.setHelpUrl("");
+    this.setTooltip("");
+    this.setHelpUrl("");
   }
 };
 
